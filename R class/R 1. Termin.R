657ccb311ce7f3f0096b0c362d09bb2fa50668a8
Huong Nguyen <lhuongnguyenn@gmail.com>
2023-04-25 12:18:55 +0200

    2.commit

--- R class/R 1. Termin.R
+++ R class/R 1. Termin.R
@@ -122,3 +122,33 @@ plot(NULL,
 for (l in 1:ncol(A)){
   lines(A[,l], type = "l", col = l)
 }
+my_fun <- function(x){
+  square <- x^2
+  return(square)
+}
+my_fun(x=5)
+
+my_fun_alternative <- function(x) x^2 
+my_fun_alternative(x=5)
+
+my_fun <- function(time, scenarios){
+  r<- scenarios 
+  A<- matrix(nrow=time, ncol=length(r), byrow=TRUE)
+  P<-1
+  for (t in 1:time){
+    for(s in 1:length(r)){
+      A[t,s] <- P*(1+r[s])^t
+    }
+  }
+plot(NULL,
+     xlim= c(1,nrow(A)),
+     ylim=c(1,max(A)), 
+     main="Compound interests", 
+     xlab="Year", 
+     ylab="Euro")
+for (l in 1:ncol(A)){
+  lines(A[,l], type="l", col=l)
+}
+}
+my_fun(time=10, scenarios=0.01)
+my_fun(time=10, scenarios=seq(0.01, 0.05, 0.01))
