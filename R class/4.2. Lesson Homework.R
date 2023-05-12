c4cf61403ab773540cf9942cf52c5d6d4213e6e2
Huong Nguyen <lhuongnguyenn@gmail.com>
2023-05-12 11:31:54 +0200

    ngzgungv

--- R class/4.2. Lesson Homework.R
+++ R class/4.2. Lesson Homework.R
@@ -6,16 +6,16 @@ library(wooldridge)
 
 View(wage1) 
 
-wage1$seperation <- ifelse(wage1$female == 0 & wage1$married == 1, "married men", 
+wage1$categories <- ifelse(wage1$female == 0 & wage1$married == 1, "married men", 
                            ifelse(wage1$female == 0 & wage1$married == 0, "single men",
                                   ifelse(wage1$female == 1 & wage1$married == 0, "single women", "married women")))
-seperation <- c("married men","single men","single women", "married women")
+categories <- c("married men","single men","single women", "married women")
 
 
-a <- mean(wage1$wage[wage1$seperation == "married men"])
-b <- mean(wage1$wage[wage1$seperation == "single men"])
-c <- mean(wage1$wage[wage1$seperation == "single women"])
-d <- mean(wage1$wage[wage1$seperation == "married women"])
+a <- mean(wage1$wage[wage1$categories == "married men"])
+b <- mean(wage1$wage[wage1$categories == "single men"])
+c <- mean(wage1$wage[wage1$categories == "single women"])
+d <- mean(wage1$wage[wage1$categories == "married women"])
 
 A <- c(a,b,c,d)
 
@@ -33,19 +33,20 @@ rm(list=ls())
 
 library(wooldridge) 
 
-wage1$seperation <- ifelse(wage1$female == 0 & wage1$married == 1, "married men", 
+wage1$categories <- ifelse(wage1$female == 0 & wage1$married == 1, "married men", 
                            ifelse(wage1$female == 0 & wage1$married == 0, "single men",
                                   ifelse(wage1$female == 1 & wage1$married == 0, "single women", "married women")))
-seperation <- c("married men","single men","single women", "married women")
+categories <- c("married men","single men","single women", "married women")
 
-Aver <- vector("numeric",length(seperation))
-for (i in 1: length(seperation)) {
- (Aver[i] <- mean(wage1$wage[wage1$seperation == seperation[i]]))
+Aver <- vector("numeric",length(categories))
+for (i in 1: length(categories)) {
+ (Aver[i] <- mean(wage1$wage[wage1$categories == categories[i]]))
 }
 
 barplot(Aver,
         names = c("married men","single men","single women", "married women"),
         col = c("lightblue","lightblue","pink","pink"),
         ylab = "Dollar per Hour",
-        ylim = c(0,10),
+        ylim = c(0,10), 
         main = "Mean Hourly Wage by Gender and Marital Status")
+
